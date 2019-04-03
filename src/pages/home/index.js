63ec474eb47cb0d0f693d67e846ea435abae75c2
Chiu Yung <1837060380@qq.com>
2019-04-03 17:16:32 +0800

    no message

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,14 +1,16 @@
 import React,{Component} from 'react'
 import Tabber from '../../components/Tabbar'
+import './index.scss'
 class Home extends Component{
-
   tabrclick(index){
 
   }
-
   render() {
     return (
       <div>
+        <div className='pageWrapper'>
+
+        </div>
         <Tabber tabBarClick={(index)=>{this.tabrclick(index)}}></Tabber>
       </div>
     );
